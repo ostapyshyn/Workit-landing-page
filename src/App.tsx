799e01de2b7a0b28d70a04713a2fa5486569ccb4
ostapyshyn <volodymyr.ostapyshyn@inventorsoft.co>
2023-07-10 11:23:27 +0300

    Add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,29 @@ function App() {
     <main>
       <p>Apply for access</p>
       <h1>Data tailored to your needs.</h1>
-
-      <h2>Actionable insights</h2>
-      <p>
-        Optimize your products, improve customer satisfaction and stay ahead of the competition with
-        our product data analytics.
-      </p>
-      <h2>Data-driven decisions</h2>
-      <p>
-        Make data-driven decisions with our product data analytics. Our AI-generated reports help
-        you unlock insights hidden in your product data.
-      </p>
-      <h2>Always affordable</h2>
-      <p>
-        Always affordable pricing that scales with your business. Get top-quality product data
-        analytics services without hidden costs or unexpected fees.
-      </p>
+      <div className={'actions'}>
+        <div className={'action_block'}>
+          <h2 className="action_title">Actionable insights</h2>
+          <p className={'about'}>
+            Optimize your products, improve customer satisfaction and stay ahead of the competition
+            with our product data analytics.
+          </p>
+        </div>
+        <div className={'action_block'}>
+          <h2 className="action_title">Data-driven decisions</h2>
+          <p className={'about'}>
+            Make data-driven decisions with our product data analytics. Our AI-generated reports
+            help you unlock insights hidden in your product data.
+          </p>
+        </div>
+        <div>
+          <h2 className="action_title">Always affordable</h2>
+          <p className={'about'}>
+            Always affordable pricing that scales with your business. Get top-quality product data
+            analytics services without hidden costs or unexpected fees.
+          </p>
+        </div>
+      </div>
 
       <h3>Be the first to test</h3>
       <p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './assets/fonts/fraunces/Fraunces-VariableFont_SOFT,WONK,opsz,wght.ttf'
+import './assets/fonts/manrope/Manrope-VariableFont_wght.ttf'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
