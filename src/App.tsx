88a227e8ebc791e80138939597a12ab7803aa2b4
ostapyshyn <volodymyr.ostapyshyn@inventorsoft.co>
2023-07-10 09:49:14 +0300

    Add text

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <p>Apply for access</p>
+      <h1>Data tailored to your needs.</h1>
+
+      <h2>Actionable insights</h2>
+      <p>
+        Optimize your products, improve customer satisfaction and stay ahead of the competition with
+        our product data analytics.
+      </p>
+      <h2>Data-driven decisions</h2>
+      <p>
+        Make data-driven decisions with our product data analytics. Our AI-generated reports help
+        you unlock insights hidden in your product data.
+      </p>
+      <h2>Always affordable</h2>
+      <p>
+        Always affordable pricing that scales with your business. Get top-quality product data
+        analytics services without hidden costs or unexpected fees.
+      </p>
+
+      <h3>Be the first to test</h3>
+      <p>
+        Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta
+        tester for our app and kickstart your company. Apply for access below and I’ll be in touch
+        to schedule a call.
+      </p>
+      <button>Apply for access</button>
+    </main>
+  )
+}
+
+export default App
